2d63c395884232985efd5e89b5a0dec9cbeb377c
Abed Nganga <ngangaabed@gmail.com>
2023-09-22 14:28:17 +0300

    Building a GET API and learnt Serialization

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ from models import db, User, Review, Game
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.json.compact = False   #configuration that has JSON responses print on separate lines with indentation. This adds some overhead, but if human eyes will be looking at your API, it's always good to have this set to True.
 
 migrate = Migrate(app, db)
 
@@ -18,6 +18,53 @@ db.init_app(app)
 @app.route('/')
 def index():
     return "Index for Game/Review/User API"
+
+
+@app.route('/games')
+def games():
+    games = []
+    for game in Game.query.all():
+        game_dict = {
+            "title": game.title,
+            "genre": game.genre,
+            "platform": game.platform,
+            "price": game.price,
+        }
+        games.append(game_dict)
+
+    response = make_response(
+        jsonify(games),  #jsonify is a method in Flask that serializes its arguments as JSON and returns a Response object...It accepts lists,dicts but not models.
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+
+@app.route('/games/<int:id>')
+def game_by_id(id):
+    game = Game.query.filter_by(id=id).first()
+
+    game_dict = game.to_dict()
+    # game_dict = {
+    #     "title": game.title,
+    #     "genre": game.genre,
+    #     "platform": game.platform,
+    #     "price": game.price,        
+    # }
+
+    response = make_response(
+        jsonify(game_dict),
+        200
+    )
+
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
